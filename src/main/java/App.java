219457781eaca3038579ec3ab6f823e20206194b
Isaac23-K <ikaptoge@yahoo.com>
2021-06-07 11:24:03 +0300

    added static file location in main class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,10 +2,17 @@ import java.util.HashMap ;
 import java.util.List;
 import java.util.Map;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
 
 
 public class App {
     public static void main(String[] args) {
-        get("/", )
+
+        staticFileLocation("/public");
+        Map<String,Object> model = new HashMap<String,Object>();
+        String layout = "templates/layout.hbs";
+
     }
 }

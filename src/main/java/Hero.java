ae072125cfd59edea31d1602f00c33ed539ce323
Isaac23-K <ikaptoge@yahoo.com>
2021-06-07 11:29:46 +0300

    created member variables for class hero

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
+    private String hero_name;
+    private int age;
+    private String powers;
+    private String weakness;
+    private static List<Hero> instance = new ArrayList<>();
+
+    public Hero(String name, int age, String powers, String weakness) {
+        this.hero_name = name;
+        this.age = age;
+        this.powers = powers;
+        this.weakness = weakness;
+    }
 }
